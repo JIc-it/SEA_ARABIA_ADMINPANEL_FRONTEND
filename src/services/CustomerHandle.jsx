e153e6af5b7e65aea47e264cf97c6741d07ab420
JIc-it <isa@job-in-arabia.com>
2023-12-19 17:14:34 +0530

    feat: Complete the Guest user api: Sandeep

--- src/services/CustomerHandle.jsx
+++ src/services/CustomerHandle.jsx
@@ -4,6 +4,8 @@ const updateVendorDetails = "account/vendor-add-details";
 const updateCustomerDetails = "account/users-update";
 const customerCount = "account/user-count-admin";
 const serviceDetails = "main/category-list";
+const guestUserURL = "account/guest-user/list"
+
 
 export const getCustomerlist = () => {
   return axiosInstance
@@ -67,6 +69,30 @@ export const getCustomerTotalCount = () => {
         "Error while fetching total count of customer request:",
         error
       );
+      throw error;
+    });
+};
+
+export const getGuestUserRequest = () => {
+  return axiosInstance
+    .get(guestUserURL)
+    .then((response) => {
+      console.log("Reward Products Response:", response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.error("Error while fetching reward products:", error);
+      throw error;
+    });
+};
+
+
+export const getTotalGuestUser = () => {
+  return axiosInstance
+    .get("account/guest-user/list")
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error("Error while fetching lead request:", error);
       throw error;
     });
 };
