eb9f235120681bc1a2d39c933e79a7a0cb4d01e3
JIc-it <isa@job-in-arabia.com>
2024-01-17 15:16:57 +0530

    fix: resolve the version issue: Sandeep

--- src/components/Service/Availability.js
+++ src/components/Service/Availability.js
@@ -1,13 +1,13 @@
 
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
-import { Card, CardContent, Checkbox, FormControlLabel, Typography } from '@material-ui/core';
+// import { Card, CardContent, Checkbox, FormControlLabel, Typography } from '@material-ui/core';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { useSpring, animated } from 'react-spring';
 import { useFormik } from "formik";
-import { format, parse } from 'date-fns'
+// import { format, parse } from 'date-fns'
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -331,9 +331,9 @@ const Availability = ({ selectedOptions, onChange, setIsRefetch, isRefetch, clos
                           }}
                           onClick={() => handleSlotClick(slot?.time)}
                         >
-                          <Typography variant="body2">
+                          {/* <Typography variant="body2">
                             {slot?.time}
-                          </Typography>
+                          </Typography> */}
                         </div>
                       ))}
                     </div>
